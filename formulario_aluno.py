778691f3c47ad153759c81bf7f7c97a955fc71ae
Pedro Marcello <113067767+pedromarcello0@users.noreply.github.com>
2024-09-18 21:45:27 -0300

    algumas alterações nas funções

--- formulario_aluno.py
+++ formulario_aluno.py
@@ -1,6 +1,6 @@
 import tkinter as tk
+from tkinter import ttk
 import sqlite3
-
 
 class FormularioAluno:
     def __init__(self, root):
@@ -9,7 +9,7 @@ class FormularioAluno:
     def criar_formulario(self):
         self.janela_form = tk.Toplevel()
         self.janela_form.title("ADICIONAR ALUNO")
-        self.janela_form.geometry("400x350")
+        self.janela_form.geometry("400x400")
         self.janela_form.config(bg='gray')
         self.janela_form.resizable(False, False)
 
@@ -17,36 +17,67 @@ class FormularioAluno:
         texto_titulo.pack(pady=10)
 
         texto_nome = tk.Label(self.janela_form, text="NOME:", bg='gray', fg='white', anchor='w')
-        texto_nome.place(x=10, y=60, width=100, height=25)
+        texto_nome.place(x=10, y=50, width=100, height=25)
 
         self.caixa_nome = tk.Entry(self.janela_form, bg='lightgrey', fg='black')
-        self.caixa_nome.place(x=120, y=60, width=250, height=25)
+        self.caixa_nome.place(x=120, y=50, width=260, height=25)
 
-        texto_info = tk.Label(self.janela_form, text="INFORMAÇÕES:", bg='gray', fg='white', anchor='w')
-        texto_info.place(x=10, y=100, width=100, height=25)
+        texto_materia = tk.Label(self.janela_form, text="MATERIA:", bg='gray', fg='white', anchor='w')
+        texto_materia.place(x=10, y=90, width=100, height=25)
 
-        self.caixa_texto = tk.Text(self.janela_form, bg='lightgrey', fg='black', wrap='word')
-        self.caixa_texto.place(x=10, y=130, width=360, height=150)
+        self.caixa_materia = tk.Text(self.janela_form, bg='lightgrey', fg='black', wrap='word')
+        self.caixa_materia.place(x=120, y=90, width=260, height=25)
 
+        texto_av1 = tk.Label(self.janela_form, text="AV1:", bg='gray', fg='white', anchor='w')
+        texto_av1.place(x=10, y=180, width=100, height=25)
+
+        self.caixa_av1 = tk.Entry(self.janela_form, bg='lightgrey', fg='black')
+        self.caixa_av1.place(x=120, y=180, width=100, height=25)
+
+        texto_av2 = tk.Label(self.janela_form, text="AV2:", bg='gray', fg='white', anchor='w')
+        texto_av2.place(x=10, y=220, width=100, height=25)
+
+        self.caixa_av2 = tk.Entry(self.janela_form, bg='lightgrey', fg='black')
+        self.caixa_av2.place(x=120, y=220, width=100, height=25)
+
+        texto_av3 = tk.Label(self.janela_form, text="AV3:", bg='gray', fg='white', anchor='w')
+        texto_av3.place(x=10, y=260, width=100, height=25)
+
+        self.caixa_av3 = tk.Entry(self.janela_form, bg='lightgrey', fg='black')
+        self.caixa_av3.place(x=120, y=260, width=100, height=25)
+
+        # Botão salvar
         button_salvar = tk.Button(self.janela_form, text="Salvar", bg='gray', fg='white', command=self.button_salvar_acao)
-        button_salvar.place(x=150, y=300, width=100, height=30)
-
+        button_salvar.place(x=150, y=310, width=100, height=30)
 
     def button_salvar_acao(self):
         conexao = sqlite3.connect('meu_banco.db')
         cursor = conexao.cursor()
 
-        termo_nome = self.caixa_nome.get().strip()  # Obtendo o valor da caixa de nome
-        termo_texto = self.caixa_texto.get("1.0", tk.END).strip()  # Obtendo o valor da caixa de texto
+        termo_nome = self.caixa_nome.get().strip()
+        termo_materia = self.caixa_materia.get("1.0", tk.END).strip()
+        termo_av1 = self.caixa_av1.get().strip()
+        termo_av2 = self.caixa_av2.get().strip()
+        termo_av3 = self.caixa_av3.get().strip()
 
         cursor.execute('''
-            INSERT INTO notas (nome, conteudo)
-            VALUES (?, ?)
-        ''', (termo_nome, termo_texto))
+            INSERT INTO notas (nome, materia, av1, av2, av3)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (termo_nome, termo_materia, termo_av1, termo_av2, termo_av3))
+
+        # Recupera o ID da linha inserida
+        id_nota = cursor.lastrowid
+
+        # Calcula a média e atualiza a tabela
+        media = self.calc_media(float(termo_av1), float(termo_av2), float(termo_av3))
+        cursor.execute('''
+            UPDATE notas
+            SET media = ?
+            WHERE id = ?
+        ''', (media, id_nota))
 
         conexao.commit()
         conexao.close()
-
         self.fechar_formulario()
 
     def fechar_formulario(self):
@@ -54,3 +85,5 @@ class FormularioAluno:
             self.janela_form.destroy()
             self.janela_form = None
 
+    def calc_media(self, av1, av2, av3):
+        return (av1 + av2 + av3) / 3
--- index.py
+++ index.py
@@ -3,8 +3,6 @@ from tkinter import ttk
 import sqlite3
 from formulario_aluno import FormularioAluno
 
-
-
 def criar_banco():
     conexao = sqlite3.connect('meu_banco.db')
     cursor = conexao.cursor()
@@ -12,80 +10,109 @@ def criar_banco():
         CREATE TABLE IF NOT EXISTS notas (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             nome TEXT NOT NULL,
-            conteudo TEXT
-        )
+            materia TEXT NOT NULL,
+            av1 REAL CHECK(av1 <= 10),
+            av2 REAL CHECK(av2 <= 10),
+            av3 REAL CHECK(av3 <= 10),
+            media REAL 
+        ) 
     ''')
     conexao.commit()
     conexao.close()
 
-def button_consulta_acao():
+
+
+def formatar_media(media):
+    return f"{media:.1f}"
+
+def button_atualizar_acao():
     conexao = sqlite3.connect('meu_banco.db')
     cursor = conexao.cursor()
 
     cursor.execute('SELECT * FROM notas')
     resultado = cursor.fetchall()
 
-    # Limpar a Treeview antes de adicionar novos dados
+    # Limpar a Treeview
     for item in tv.get_children():
         tv.delete(item)
 
-    # Inserir novos dados na Treeview
     for item in resultado:
-        tv.insert('', tk.END, values=item)
+        id_nota, nome, materia, av1, av2, av3, media = item
+        media_formatada = formatar_media(media) if media is not None else 'N/A'
+        tv.insert('', tk.END, values=(id_nota, nome, materia, av1, av2, av3, media_formatada))
 
+    conexao.close()
 
 def button_criar_acao():
     formulario = FormularioAluno(janela)
     formulario.criar_formulario()
 
 def button_pesquisar_acao():
-
     conexao = sqlite3.connect('meu_banco.db')
     cursor = conexao.cursor()
 
-    termo_pesquisa = texto_pesquisa.get("1.0", tk.END).strip() 
+    termo_pesquisa = texto_pesquisa.get("1.0", tk.END).strip()
 
     cursor.execute('SELECT * FROM notas WHERE nome LIKE ?', ('%' + termo_pesquisa + '%',))
     resultado = cursor.fetchall()
 
-    # Limpar a Treeview antes de adicionar
+    # Limpar a Treeview 
     for item in tv.get_children():
         tv.delete(item)
 
-    # Inserir dados na Treeview
+    # Inserir dados Treeview
     for item in resultado:
-        tv.insert('', tk.END, values=item)
+        id_nota, nome, materia, av1, av2, av3, media = item
+        tv.insert('', tk.END, values=(id_nota, nome, materia, av1, av2, av3, media))
 
-    conexao.commit()
     conexao.close()
 
 def button_editar_acao():
     pass
 
+ 
+def button_remover_acao():
+    pass
+    
+
 
 # Criar a janela principal
 janela = tk.Tk()
 janela.title('APP ALUNOS')
-janela.geometry('500x380')
+janela.geometry('700x500')  
 janela.config(bg='gray')
 janela.resizable(False, False)
 
-#Treeview para consulta
-tv = ttk.Treeview(janela, columns=('id', 'nome', 'conteudo'), show='headings')
-tv.column('id', minwidth=0, width=10)
-tv.column('nome', minwidth=0, width=150)
-tv.column('conteudo', minwidth=0, width=150)
-tv.heading('id', text='ID')
-tv.heading('nome', text='NOME')
-tv.heading('conteudo', text='CONTEÚDO')
-tv.place(x=100, y=10, width=390, height=360)
+# Treeview para consulta
+tv = ttk.Treeview(janela, columns=('id', 'nome', 'materia', 'av1', 'av2', 'av3', 'media'), show='headings')
+tv.column('id', minwidth=0, width=50, anchor='center')
+tv.column('nome', minwidth=0, width=150, anchor='center')
+tv.column('materia', minwidth=0, width=150, anchor='center')
+tv.column('av1', minwidth=0, width=50, anchor='center')
+tv.column('av2', minwidth=0, width=50, anchor='center')
+tv.column('av3', minwidth=0, width=50, anchor='center')
+tv.column('media', minwidth=0, width=50, anchor='center')
+tv.heading('id', text='ID', anchor='center')
+tv.heading('nome', text='NOME', anchor='center')
+tv.heading('materia', text='MATERIA', anchor='center')
+tv.heading('av1', text='AV1', anchor='center')
+tv.heading('av2', text='AV2', anchor='center')
+tv.heading('av3', text='AV3', anchor='center')
+tv.heading('media', text='MEDIA', anchor='center')
+tv.place(x=100, y=10, width=580, height=470)
+
+# Criar o banco de dados quando iniciar
+criar_banco()
+
+# Atualizar a Treeview quando iniciar 
+button_atualizar_acao()
 
 # Botão consultar
-button_consulta = tk.Button(janela, text='CONSULTAR', command=button_consulta_acao, bg='lightgrey', fg='black', width=15, height=2)
-button_consulta.place(x=10, y=10, width=80, height=30)
+button_atualizar = tk.Button(janela, text='ATUALIZAR', command=button_atualizar_acao, bg='lightgrey', fg='black', width=15, height=2)
+button_atualizar.place(x=10, y=10, width=80, height=30)
 
 # Botão criar
-button_criar = tk.Button(janela, text='ADICIONAR', command=button_criar_acao, bg='lightgrey', fg='black', width=15, height=2)
+button_criar = tk.Button(janela, text='CRIAR', command=button_criar_acao, bg='lightgrey', fg='black', width=15, height=2)
 button_criar.place(x=10, y=45, width=80, height=30)
 
 # Botão pesquisar
@@ -95,5 +122,13 @@ button_pesquisar.place(x=10, y=80, width=80, height=30)
 # Caixa de texto para pesquisa
 texto_pesquisa = tk.Text(janela, bg='lightgrey', fg='black', width=20, height=2)
 texto_pesquisa.place(x=10, y=110, width=80, height=20)
+
+# Botão editar
+button_editar = tk.Button(janela, text='EDITAR', command=button_editar_acao, bg='lightgrey', fg='black', width=15, height=2)
+button_editar.place(x=10, y=135, width=80, height=30)
+
+# Botão remover
+button_remover = tk.Button(janela, text='REMOVER', command=button_remover_acao, bg='lightgrey', fg='black', width=15, height=2)
+button_remover.place(x=10, y=170, width=80, height=30)
 
 janela.mainloop()
